4ea0329dcbc1d3e35ec2d60d75a9c9b98f79553b
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-03-03 22:42:52 +0300

    Add 'Sign In' and 'Sign Up' pages

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+import { Form } from "../Form/Form";
+import s from "../SignIn/SignIn.module.scss";
+
 export const SignIn = () => {
-  return <div>SignIn page</div>;
+  return (
+    <div className={s.container}>
+      <Form name="Sign In" />
+      <p className={s.text}>Don't have an account yet?</p>
+      <Link to="/signup" className={s.link}>
+        Sign Up
+      </Link>
+    </div>
+  );
 };
