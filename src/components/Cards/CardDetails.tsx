d0a6b3584bf4ee7dfde6c2d5c948c80e2b654632
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-03-08 19:24:38 +0300

    Edit description on card page

--- src/components/Cards/CardDetails.tsx
+++ src/components/Cards/CardDetails.tsx
@@ -8,28 +8,26 @@ export const CardDetails = () => {
   const id = Number(paramId);
   const { data: item } = useGetCollectionItemByIdQuery(id);
 
+  const fieldsToShow = ["medium", "culture", "period", "date"];
+
+  const descriptionList = fieldsToShow.map(
+    (field) =>
+      item &&
+      item[field as keyof typeof item] !== "" && (
+        <p className={s.description}>
+          <span className={s.field_title}>{field}:</span>{" "}
+          {item[field as keyof typeof item]}
+        </p>
+      )
+  );
+
   return (
     <>
       {item && (
         <div className={s.container}>
           <div className={s.text_part}>
             <p className={s.title}>{item.title}</p>
-            <p className={s.description}>
-              <span className={s.field_title}>Medium:</span> {item.medium}
-            </p>
-            <p className={s.description}>
-              <span className={s.field_title}>Culture:</span> {item.culture}
-            </p>
-            {item.period !== "" && (
-              <p className={s.description}>
-                <span className={s.field_title}>Period:</span> {item.period}
-              </p>
-            )}
-            {item.date !== "" && (
-              <p className={s.description}>
-                <span className={s.field_title}>Date:</span> {item.date}
-              </p>
-            )}
+            {descriptionList}
           </div>
           <div className={s.image_wrapper}>
             <img className={s.image} src={item.image} alt={item.title} />
