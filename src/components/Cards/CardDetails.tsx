467e7b3db355b1934ce6a7a76ef402a164f3de44
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-03-08 14:51:58 +0300

    Fix description on card page

--- src/components/Cards/CardDetails.tsx
+++ src/components/Cards/CardDetails.tsx
@@ -1,45 +1,12 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { useGetCollectionItemByIdQuery } from "../../services/collectionApi";
 import s from "../Cards/CardDetails.module.scss";
 
 export const CardDetails = () => {
-  let { id: paramId } = useParams();
-  const [fields, setFields] = useState<Array<string>>([]);
+  const { id: paramId } = useParams();
 
   const id = Number(paramId);
   const { data: item } = useGetCollectionItemByIdQuery(id);
-
-  const fieldsToShow = ["medium", "culture", "period", "date", "country"];
-
-  function getFilledFields() {
-    const fields = [];
-
-    if (item !== undefined) {
-      for (const [key, value] of Object.entries(item)) {
-        if (value !== "") {
-          fields.push(key);
-        }
-      }
-    }
-
-    setFields(fields);
-  }
-
-  useEffect(() => {
-    getFilledFields();
-  }, []);
-
-  const descriptionList = fields.map(
-    (field) =>
-      item &&
-      fieldsToShow.includes(field) && (
-        <p className={s.description}>
-          <span className={s.field_title}>{field}:</span>{" "}
-          {item[field as keyof typeof item]}
-        </p>
-      )
-  );
 
   return (
     <>
@@ -47,7 +14,22 @@ export const CardDetails = () => {
         <div className={s.container}>
           <div className={s.text_part}>
             <p className={s.title}>{item.title}</p>
-            {descriptionList}
+            <p className={s.description}>
+              <span className={s.field_title}>Medium:</span> {item.medium}
+            </p>
+            <p className={s.description}>
+              <span className={s.field_title}>Culture:</span> {item.culture}
+            </p>
+            {item.period !== "" && (
+              <p className={s.description}>
+                <span className={s.field_title}>Period:</span> {item.period}
+              </p>
+            )}
+            {item.date !== "" && (
+              <p className={s.description}>
+                <span className={s.field_title}>Date:</span> {item.date}
+              </p>
+            )}
           </div>
           <div className={s.image_wrapper}>
             <img className={s.image} src={item.image} alt={item.title} />
