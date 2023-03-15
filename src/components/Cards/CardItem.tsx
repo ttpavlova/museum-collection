99a85a2ed4ab8a8a4542ec575d33e0ac0daa55fa
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-03-15 15:26:02 +0300

    Add classnames package

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useGetCollectionItemByIdQuery } from "../../services/collectionApi";
 import { ThemeContext } from "../../ThemeProvider";
+import classNames from "classnames";
 import s from "../Cards/CardItem.module.scss";
 
 interface CardItemProps {
@@ -12,15 +13,17 @@ export const CardItem = ({ id }: CardItemProps) => {
   const { data: item } = useGetCollectionItemByIdQuery(id);
   const { theme } = useContext(ThemeContext);
 
+  const containerClass = classNames(s.container, {
+    [s.beige_theme]: theme === "beige",
+  });
+
   return (
     <div>
       {item && (
         <Link
           to={`/search/${item.id}`}
           title={`${item.title} Card Page`}
-          className={
-            s.container + (theme === "light" ? "" : " " + s.beige_theme)
-          }
+          className={containerClass}
         >
           <div className={s.image_wrapper}>
             <img className={s.image} src={item.image} alt={item.title}></img>
