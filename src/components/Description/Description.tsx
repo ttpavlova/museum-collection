2cda9bc7a313bcbec951e6fdcabc579b602eb9fd
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-03-15 14:30:51 +0300

    Change description on main page

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -7,7 +7,8 @@ export const Description = () => {
       <h1 className={s.header_text}>The Met Collection</h1>
       <div className={s.description_text}>
         <p className={s.text}>
-          Explore works of Chinese Sculpture from The Met's Collection.
+          Explore works of Chinese Sculpture from The Metropolitan Museum of
+          Art's Collection.
         </p>
         <p className={s.text}>Examples to search: pagoda, teapot, mountain.</p>
       </div>
